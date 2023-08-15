6e9dc1eedb53d6ca0e5587a93d74e3748ff31a5d
LPFreitas <lfreitasp2001@gmail.com>
2023-08-15 02:05:50 +0200

    Versão inteiros funcionando

---
+++ Makefile
@@ -0,0 +1,52 @@
+# Makefile
+FONTE_AS = output.s
+FONTE_C = compiler.c
+
+# Arquivos de saída 
+EXEC_AS = kernel.elf
+EXEC_C = compiler
+MAP = kernel.map
+
+PREFIXO = arm-none-eabi-
+LDSCRIPT = kernel.ld
+AS = ${PREFIXO}as
+LD = ${PREFIXO}ld
+OBJETO_AS = $(FONTE_AS:.s=.o)
+
+all: ${EXEC_C} ${FONTE_AS} ${EXEC_AS}
+
+# Gerar executável C
+${EXEC_C}: ${FONTE_C}
+	gcc $(FONTE_C) -o ${EXEC_C}
+
+# Executar executável C
+${FONTE_AS}: ${EXEC_C}
+	./${EXEC_C}
+
+# Gerar executável AS
+${EXEC_AS}: ${OBJETO_AS} ${EXEC_C}
+	${LD} -T ${LDSCRIPT} -M=${MAP} -o $@ $<
+
+# Montar arquivos em assembler
+.s.o:
+	${AS} -g -o $@ $<
+
+# Limpar tudo
+clean:
+	rm -f ${FONTE_AS} ${OBJETO_AS} ${EXEC_AS} ${EXEC_C} ${MAP}
+
+# Iniciar qemu
+qemu: ${EXEC_AS}
+	@if lsof -Pi :1234 >/dev/null ; then\
+		echo "qemu já está executando"; \
+	else qemu-system-arm -s -M virt -kernel ${EXEC_AS} & \
+	fi
+
+# Iniciar gdb
+gdb: ${EXEC_AS}
+	gdb-multiarch -ex "set architecture arm" \
+		          -ex "target extended-remote :1234" \
+				  -ex "load" \
+				  -ex "layout src" \
+				  -ex "layout regs" \
+				  ${EXEC_AS}
--- compiler.c
+++ compiler.c
@@ -7,21 +7,32 @@
 
 FILE *outputFile;
 
-struct Stack {
+struct OperatorStack
+{
     char data[MAX_SIZE];
     int top;
 };
 
-void initialize(struct Stack *stack) {
+struct OperandStack
+{
+    int data[MAX_SIZE];
+    int top;
+};
+
+void initializeOperatorStack(struct OperatorStack *stack)
+{
     stack->top = -1;
 }
 
-int isEmpty(struct Stack *stack) {
+int isEmptyOperatorStack(struct OperatorStack *stack)
+{
     return stack->top == -1;
 }
 
-void push(struct Stack *stack, char value) {
-    if (stack->top == MAX_SIZE - 1) {
+void pushOperatorStack(struct OperatorStack *stack, char value)
+{
+    if (stack->top == MAX_SIZE - 1)
+    {
         printf("Erro: pilha cheia\n");
         exit(1);
     }
@@ -29,173 +40,252 @@ void push(struct Stack *stack, char value) {
     stack->data[stack->top] = value;
 }
 
-char pop(struct Stack *stack) {
-    if (isEmpty(stack)) {
+char popOperatorStack(struct OperatorStack *stack)
+{
+    if (isEmptyOperatorStack(stack))
+    {
         printf("Erro: pilha vazia\n");
         exit(1);
     }
     return stack->data[stack->top--];
 }
 
-char peek(struct Stack *stack) {
-    if (isEmpty(stack)) {
+char peekOperatorStack(struct OperatorStack *stack)
+{
+    if (isEmptyOperatorStack(stack))
+    {
         printf("Erro: pilha vazia\n");
         exit(1);
     }
     return stack->data[stack->top];
 }
 
-int isOperator(char c) {
+void initializeOperandStack(struct OperandStack *stack)
+{
+    stack->top = -1;
+}
+
+int isEmptyOperandStack(struct OperandStack *stack)
+{
+    return stack->top == -1;
+}
+
+void pushOperandStack(struct OperandStack *stack, int value)
+{
+    if (stack->top == MAX_SIZE - 1)
+    {
+        printf("Erro: pilha cheia\n");
+        exit(1);
+    }
+    stack->top++;
+    stack->data[stack->top] = value;
+}
+
+int popOperandStack(struct OperandStack *stack)
+{
+    if (isEmptyOperandStack(stack))
+    {
+        printf("Erro: pilha vazia\n");
+        exit(1);
+    }
+    return stack->data[stack->top--];
+}
+
+void printOperandStack(struct OperandStack *stack)
+{
+    int i = 0;
+    printf("PILHA:\n");
+    while (i <= stack->top)
+        printf("%d", stack->data[i++]);
+    printf("\n\n");
+}
+
+int isOperator(char c)
+{
     return (c == '+' || c == '-' || c == '*' || c == '/');
 }
 
-int precedence(char op) {
-    if (op == '+' || op == '-') {
+int precedence(char op)
+{
+    if (op == '+' || op == '-')
         return 1;
-    } else if (op == '*' || op == '/') {
+    else if (op == '*' || op == '/')
         return 2;
-    }
+
     return 0;
 }
 
-void infixToPostfix(char *infix, char *postfix) {
-    struct Stack operatorStack;
-    initialize(&operatorStack);
+void infixToPostfix(char *infix, char *postfix)
+{
+    struct OperatorStack operatorStack;
+    initializeOperatorStack(&operatorStack);
 
     int length = strlen(infix);
     int postfixIndex = 0;
 
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++)
+    {
         char c = infix[i];
 
-        if (isspace(c)) {
+        if (isspace(c))
             continue; // Ignorar espaços em branco
-        } else if (isdigit(c)) {
+        else if (isdigit(c))
+        {
             // Para números com mais de um algarismo
-            while (i < length && (isdigit(infix[i]))) {
+            while (i < length && (isdigit(infix[i])))
                 postfix[postfixIndex++] = infix[i++];
-            }
-            postfix[postfixIndex++] = ' ';
-            i--; // Decrementar i pois o loop principal também incrementa
 
-        } else if (c == '-' && isdigit(infix[i+1])) {
-            postfix[postfixIndex++] = infix[i++];
-            while (i < length && (isdigit(infix[i]))) {
-                postfix[postfixIndex++] = infix[i++];
-            }
             postfix[postfixIndex++] = ' ';
             i--; // Decrementar i pois o loop principal também incrementa
-        } else if (c == '(') {
-            push(&operatorStack, c);
-        } else if (c == ')') {
-            while (!isEmpty(&operatorStack) && peek(&operatorStack) != '(') {
-                postfix[postfixIndex++] = pop(&operatorStack);
+        }
+        else if (c == '-' && isdigit(infix[i + 1]))
+        {
+            postfix[postfixIndex++] = infix[i++];
+            while (i < length && (isdigit(infix[i])))
+                postfix[postfixIndex++] = infix[i++];
+
+            postfix[postfixIndex++] = ' ';
+            i--; // Decrementar i pois o loop principal também incrementa
+        }
+        else if (c == '(')
+            pushOperatorStack(&operatorStack, c);
+        else if (c == ')')
+        {
+            while (!isEmptyOperatorStack(&operatorStack) && peekOperatorStack(&operatorStack) != '(')
+            {
+                postfix[postfixIndex++] = popOperatorStack(&operatorStack);
+                postfix[postfixIndex++] = ' ';
             }
-            pop(&operatorStack);
-        } else if (isOperator(c)) {
-            while (!isEmpty(&operatorStack) && precedence(peek(&operatorStack)) >= precedence(c)) {
-                postfix[postfixIndex++] = pop(&operatorStack);
+
+            popOperatorStack(&operatorStack);
+        }
+        else if (isOperator(c))
+        {
+            while (!isEmptyOperatorStack(&operatorStack) && precedence(peekOperatorStack(&operatorStack)) >= precedence(c))
+            {
+                postfix[postfixIndex++] = popOperatorStack(&operatorStack);
+                postfix[postfixIndex++] = ' ';
             }
-            push(&operatorStack, c);
+
+            pushOperatorStack(&operatorStack, c);
         }
     }
 
-    while (!isEmpty(&operatorStack)) {
-        postfix[postfixIndex++] = pop(&operatorStack);
+    while (!isEmptyOperatorStack(&operatorStack))
+    {
+        postfix[postfixIndex++] = popOperatorStack(&operatorStack);
+        postfix[postfixIndex++] = ' ';
     }
 
+    postfixIndex--; // Decrementar postfixIndex para sobrescrever espaço a mais
     postfix[postfixIndex] = '\0';
 }
 
-void generateARMCode(char *postfix) {
+void generateARMCode(char *postfix)
+{
     // Gerar código Assembly para armazenar o resultado em um registrador e imprimir
     outputFile = fopen("output.s", "w");
-    if (!outputFile) {
+    if (!outputFile)
+    {
         printf("Erro ao criar o arquivo de saída\n");
         exit(1);
     }
 
     fprintf(outputFile, ".text\n.global main\nmain:\n");
+    fprintf(outputFile, "\tldr sp, =inicio_stack\n");
 
-    struct Stack operandStack;
-    initialize(&operandStack);
+    struct OperandStack operandStack;
+    initializeOperandStack(&operandStack);
 
     int length = strlen(postfix);
-    int ARMStackLength = 0;
 
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++)
+    {
         char c = postfix[i];
 
-        if (isdigit(c)) {
+        if (isdigit(c))
+        {
             int num = 0;
-            while (i < length && (isdigit(postfix[i]))) {
+            while (i < length && (isdigit(postfix[i])))
+            {
                 num = num * 10 + (postfix[i] - '0');
                 i++;
             }
             i--; // Decrementar i pois o loop principal também incrementa
 
-            push(&operandStack, num);
-            ARMStackLength += 1;
+            pushOperandStack(&operandStack, num);
             fprintf(outputFile, "\tmov r0, #%d\n", num);
-            fprintf(outputFile, "\tstr r0, [sp, #-%d]!\n", ARMStackLength * 4);
-        } else if (c == '-' && isdigit(postfix[i + 1])) {
+            fprintf(outputFile, "\tpush {r0}\n");
+        }
+        else if (c == '-' && isdigit(postfix[i + 1]))
+        {
             int num = 0;
             i++;
-            while (i < length && (isdigit(postfix[i]))) {
+            while (i < length && (isdigit(postfix[i])))
+            {
                 num = num * 10 + (postfix[i] - '0');
                 i++;
             }
             num = -num;
             i--; // Decrementar i pois o loop principal também incrementa
 
-            push(&operandStack, num);
-            ARMStackLength += 1;
+            pushOperandStack(&operandStack, num);
             fprintf(outputFile, "\tmov r0, #%d\n", num);
-            fprintf(outputFile, "\tstr r0, [sp, #-%d]!\n", ARMStackLength * 4);
-        } else if (isOperator(c)) {
+            fprintf(outputFile, "\tpush {r0}\n");
+        }
+        else if (isOperator(c))
+        {
             // Realiza a operação usando os dois operandos do topo da pilha
-            int operand2 = pop(&operandStack);
-            fprintf(outputFile, "\tldr r1, [sp], #%d\n", ARMStackLength * 4);
-            ARMStackLength -= 1;
-            int operand1 = pop(&operandStack);
-            fprintf(outputFile, "\tldr r0, [sp], #%d\n", ARMStackLength * 4);
-            ARMStackLength -= 1;
+            int operand2 = popOperandStack(&operandStack);
+            fprintf(outputFile, "\tpop {r1}\n");
+            int operand1 = popOperandStack(&operandStack);
+            fprintf(outputFile, "\tpop {r0}\n");
             int result;
 
-            if (c == '+') {
+            if (c == '+')
+            {
                 result = operand1 + operand2;
                 fprintf(outputFile, "\tadd r0, r0, r1\n");
-            } else if (c == '-') {
+            }
+            else if (c == '-')
+            {
                 result = operand1 - operand2;
                 fprintf(outputFile, "\tsub r0, r0, r1\n");
-            } else if (c == '*') {
+            }
+            else if (c == '*')
+            {
                 result = operand1 * operand2;
-                fprintf(outputFile, "\tmul r0, r0, r1\n");
-            } else if (c == '/') {
+                fprintf(outputFile, "\tmul r0, r1, r0\n");
+            }
+            else if (c == '/')
+            {
                 result = operand1 / operand2;
-                fprintf(outputFile, "\tdiv r0, r0, r1\n");
+                fprintf(outputFile, "\tsdiv r0, r0, r1\n");
             }
 
+            printf("> Operação %d %c %d = %d\n", operand1, c, operand2, result);
+
             // Empilha o resultado de volta na pilha
-            push(&operandStack, result);
-            ARMStackLength += 1;
-            fprintf(outputFile, "\tstr r0, [sp, #-%d]!\n", ARMStackLength * 4);
+            pushOperandStack(&operandStack, result);
+            fprintf(outputFile, "\tpush {r0}\n");
         }
     }
 
     // O resultado final estará no topo da pilha
-    printf("resultado: %d\n", pop(&operandStack));
-    
-    fprintf(outputFile, "\tldr r0, [sp], #4\n");
-    fprintf(outputFile, "fim:\n");
-    fprintf(outputFile, "\tb fim\n");
+    printf("Resultado: %d\n", popOperandStack(&operandStack));
+
+    fprintf(outputFile, "\tpop {r0}\n");
+    fprintf(outputFile, "end:\n");
+    fprintf(outputFile, "\tb end\n");
 
     fclose(outputFile);
 }
 
-int main() {
+int main()
+{
     FILE *inputFile = fopen("input.txt", "r");
-    if (!inputFile) {
+    if (!inputFile)
+    {
         printf("Erro ao abrir o arquivo de entrada\n");
         return 1;
     }
